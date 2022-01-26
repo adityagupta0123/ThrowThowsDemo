31c558c4fd9c448afaacbf49c1a9651b1a418168
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-01-26 18:02:38 +0530

    use throw and throws

--- src/Exception/demo.java
+++ src/Exception/demo.java
@@ -2,7 +2,7 @@ package Exception;
 
 public class demo {
     static int meth1(){
-        return 10/0;
+        return 10/5;
     }
     static void meth2(){
         meth1();
@@ -10,9 +10,21 @@ public class demo {
     static void meth3(){
         meth2();
     }
+    static int area (int l, int b) throws Exception {
+        if(l<0 || b<0)
+            throw new Exception();
+        return l*b;
+    }
+    static void meth4() throws Exception{
+        System.out.println("Area of rectangle "+ area(-10,5));
+    }
     public static void main(String[] args) {
         try {
             meth3();
+            meth4();
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
         }
         catch (Exception e) {
             System.out.println(e);
